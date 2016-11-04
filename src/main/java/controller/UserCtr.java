d7672c370377a6f44f65a85d5ce3c7d843eadad0
lizhenao <hzlizhentao@corp.netease.com>
2016-11-04 19:12:25 +0800

    修正了bug

--- src/main/java/controller/UserCtr.java
+++ src/main/java/controller/UserCtr.java
@@ -22,12 +22,3 @@ public class UserCtr extends Controller {
         userSer.addUser("limingming3");
     }
 };
-void  UserCtrTest()
-        {
-            int resut = addUser(0);
-        assert resut == 0;
-        int resut = addUser(1);
-        assert resut == 0;
-        int resut = addUser(2);
-        assert resut == 1;
-        }
